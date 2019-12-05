df7429f2929b01c64a374d39680028b946a1960d
igorsilva3 <igorbr2018@gmail.com>
2019-12-05 15:33:08 -0300

    Arquivo inicial

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <form action="inc/sms.php" method="post">
+        <input type="number" name="telefone" min='0'>
+        <input type="submit" value="Enviar">
+    </form>
+ </body>
+</html>
