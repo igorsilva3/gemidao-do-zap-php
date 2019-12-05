c2a24625a82bd8a0967ffc121849d05e86819f34
igorsilva3 <igorbr2018@gmail.com>
2019-12-05 19:46:30 -0300

    Arquivo de configuração

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,14 @@
 <?php
 
+//Define a chave da API
 define("API_TOKEN", "5fcf4b522b19838568c8a63e0cfa083c");
+
+//Define o metodo POST
+define('METHOD_SEND', 'POST');
+
+//Define o metodo GET
+define('METHOD_CATCH', 'GET');
+
+
 
 ?>
