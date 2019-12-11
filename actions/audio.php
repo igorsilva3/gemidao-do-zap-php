5c0e8d41075a08761c23bd6e0e06b6c2444e8a19
igorsilva3 <igorbr2018@gmail.com>
2019-12-11 18:52:05 -0300

    Adicao do retorno da API no alerta

--- actions/audio.php
+++ actions/audio.php
@@ -13,11 +13,11 @@
     //Faz a chamada da função, passando os parâmetros
     $resposta = $Comunicacao->audio($phone);
 
+    // Transforma o JSON em um objeto
+    $resultado = json_decode($resposta);
+
     echo ("<SCRIPT LANGUAGE='JavaScript'>
-        window.alert('Sucesso!')
+        window.alert('". $resultado->mensagem ."')
         window.location.href='../pages/audio.html';
         </SCRIPT>");
-    
-    //Exibe a resposta da API
-    echo $resposta;
 ?>
